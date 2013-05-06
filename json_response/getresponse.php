c9ae55ec478d15a890e7cf96d3e0e56e0d293d77
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2013-05-06 18:21:35 +0900

    クラスのシリアライズテスト。 さすがにJavaScript形式への変換は無理っぽい。

--- json_response/getresponse.php
+++ json_response/getresponse.php
@@ -2,11 +2,34 @@
 //	JavaScriptからの呼び出しに対してJSON形式のデータを返す
 
 
+	//	PHP5.4以降から使用可能な「JsonSerializable」
+		//	JSONへのシリアライズを明示するためのクラス
+	//	PHPのクラスの内容をJavaScript形式にできるかと思ったが動作が違う
+	class	Test implements JsonSerializable
+	{
+		private	$param;
+
+		function	__construct()
+		{
+			$this->param = 12345;
+			$this->value = 987;
+		}
+
+		//	この関数の戻り値がJSON形式にシリアライズされる
+		function	jsonSerialize()
+		{
+			return	$this->param;
+		}
+
+	}
+
 	$data = array(
 		'text' => 'こんにちは',
 		'value' => 5,
 		'url' => 'http://google.com',
+		'object' => new Test(),
 	);
+
 
 	$json_data = json_encode($data);
 
